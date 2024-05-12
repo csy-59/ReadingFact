426c8783211e9a8f8c2793bf6d5e5f17a44d0f31
YB970902 <78996787+YB970902@users.noreply.github.com>
2024-05-12 22:19:29 +0900

    [Feat] Success, Fail Screen 개발

---
+++ ReadingFact!/Assets/Scripts/UI/UISuccessScreen.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISuccessScreen : UIBase
+{
+    [SerializeField] private TMP_Text txtReason;
+    [SerializeField] private Button btnNext;
+
+    private void Start()
+    {
+        UIHelper.AddButtonEvent(btnNext, Close);
+    }
+
+    public void Set(SDClue sdClue)
+    {
+        txtReason.text = sdClue.Content;
+    }
+}
